22da46d218cb1cafca0e18416f2e34dec7839eb8
kazola <kazola>
2022-09-16 12:25:08 -0400

    more

--- dt_install_aws_cli_v1.sh
+++ dt_install_aws_cli_v1.sh
@@ -9,6 +9,10 @@ clear && echo && set -e
 trap 'echo ‘$BASH_COMMAND’ TRAPPED! rv $?' EXIT
 
 
+# unzip our file in _dt_files
+cd _dt_files && unzip awscli-bundle.zip
+
+
 # install it
 echo; echo 'I > install AWS cli v1'
 sudo ./_dt_files/awscli-bundle/install -i /usr/local/aws -b /usr/local/bin/aws
