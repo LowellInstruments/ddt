a0fee2c1c840e5a9c90d659fb5afc7a2fad21bbd
kazola <kazola>
2022-09-16 12:40:47 -0400

    out awscli

--- dt_install_linux.sh
+++ dt_install_linux.sh
@@ -18,7 +18,7 @@ sudo apt-get update
 sudo apt-get -y install xscreensaver matchbox-keyboard ifmetric joe git \
 libatlas3-base libglib2.0-dev python3-pyqt5 libhdf5-dev python3-dev \
 libgdal-dev libproj-dev proj-data proj-bin python3-gdbm python3-venv \
-libcurl4-gnutls-dev gnutls-dev python3-pycurl
+libcurl4-gnutls-dev gnutls-dev python3-pycurl awscli
 
 
 printf 'I > juice4halt\n'
