076f05bfbe0e48a9474fbf0c7ea5c05094f1e0ff
kazola <kazola>
2022-11-04 09:14:14 -0400

    more

--- dt_uninstall_python_all_mat_ddh.sh
+++ dt_uninstall_python_all_mat_ddh.sh
@@ -10,18 +10,19 @@ echo
 read -r -p "This will uninstall DDH. Are you sure? [y/N] " choice
 if [ "$choice" != "y" ]; then
     printf 'not doing it! bye\n'
-    exit 0
+    exit 1
 fi
 
 
 
 # uninstalling
-printf '[ DDH ] activating virtualenv to uninstall lowell-mat... \n'
+printf '[ DDH ] activating virtualenv to uninstall lowell-mat \n'
 source "$VENV"/bin/activate
-printf '[ DDH ] uninstalling lowell-mat... \n'
+printf '[ DDH ] uninstalling lowell-mat \n'
 "$VPIP" uninstall lowell-mat || true
 if [ ! -d "$F_DA" ]; then
     printf "[ DDH ] app folder does not exist, skipping removal \n"
+    exit 2
 fi
 printf '[ DDH ] uninstalling DDH application... \n'
 rm -rf "$F_DA" || true
