9f1ae0a29f97a855ad73eb4811d06c494c055dd6
kazola <kazola>
2022-11-04 09:57:27 -0400

    netservice

--- dt_install_service_sw_net.sh
+++ dt_install_service_sw_net.sh
@@ -13,10 +13,10 @@ trap 'echo "$BASH_COMMAND" TRAPPED! rv $?' EXIT
 printf '\n\n\n---- Install SW-NET service ----\n'
 
 
-printf '[ DDH ] ensuring permissions ifmetric \n'
+printf '[ DDH ] ensuring permissions ifmetric \n\n'
 sudo setcap 'cap_net_raw,cap_net_admin+eip' /usr/sbin/ifmetric
 
-printf 'I > LI switch_net service \n'
+printf '[ DDH ] installing LI switch_net service \n\n'
 sudo systemctl stop unit_switch_net.service || true
 sudo cp "$F_DT"/_dt_files/unit_switch_net.service /etc/systemd/system/
 sudo chmod 644 /etc/systemd/system/unit_switch_net.service
@@ -28,3 +28,4 @@ sudo systemctl start unit_switch_net.service
 
 echo '[ DDH ] checking status of the service right now'
 systemctl status unit_switch_net.service
+echo
