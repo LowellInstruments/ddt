ced6f1751a9f5a3b78a8205d2634859bf2c78007
kazola <kazola>
2022-11-08 10:58:06 -0500

    venv

--- dt_install_python_venv.sh
+++ dt_install_python_venv.sh
@@ -14,6 +14,7 @@ if [ "$PWD" != "$F_DT" ]; then echo 'wrong starting folder'; exit 1; fi
 # on RPi, venv needs to inherit PyQt5 installed via apt
 printf 'I > virtualenv \n'
 rm -rf "$VENV" || true
+rm -rf "$HOME"/.cache/pip || true
 python3 -m venv "$VENV" --system-site-packages
 source "$VENV"/bin/activate
 "$VPIP" install --upgrade pip
