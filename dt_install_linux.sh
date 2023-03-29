035d88b9d9f0300fa7f2d7b555a74ba1de411c44
kazola <kazola>
2023-03-29 11:40:02 -0400

    more

--- dt_install_linux.sh
+++ dt_install_linux.sh
@@ -21,12 +21,12 @@ sudo apt-mark hold bluez
 
 
 printf '\n\n>>> removing modemmanager\n'
-sudo apt remove modemmanager
+sudo apt remove -y modemmanager
 
 
 printf '\n\n>>> running linux apt-get\n'
-sudo apt-get update
-sudo apt remove python3-numpy
+sudo apt-get --yes update
+sudo apt remove -y python3-numpy
 sudo apt-get --yes --force-yes install xscreensaver matchbox-keyboard ifmetric joe git \
 libatlas3-base libglib2.0-dev python3-pyqt5 libhdf5-dev python3-dev \
 libgdal-dev libproj-dev proj-data proj-bin python3-gdbm python3-venv \
