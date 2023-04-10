b79bad243d43081e46ff5de6c8c6ce603febc22b
kazola <kazola>
2023-04-10 08:57:24 -0400

    more

--- _dt_files/ble_dl_moana.py
+++ _dt_files/ble_dl_moana.py
@@ -6,17 +6,29 @@ import os
 import re
 import struct
 import time
+import traceback
+
 from bleak import BleakClient, BleakError
 from datetime import datetime
 from enum import Enum
 
-from utils.ddh_shared import get_ddh_folder_path_dl_files, get_dl_folder_path_from_mac, create_folder_logger_by_mac
+from dds.emolt import (
+    file_moana_raw_csv_to_emolt_csv,
+    ddh_is_emolt_box,
+    file_emolt_csv_to_hl,
+    file_out_hl_process_xc_85,
+)
+from dds.rbl import rbl_build_emolt_msg_as_str, rbl_gen_file, rbl_hex_str_to_hex_bytes
+from mat.utils import linux_is_rpi
+from utils.ddh_shared import (
+    create_folder_logger_by_mac,
+)
 from utils.logs import lg_dds as lg
-from mat.ble.ble_mat_utils import ble_mat_progress_dl, ael
+from mat.ble.ble_mat_utils import ble_mat_progress_dl
 
 
-VSP_RX_CHAR_UUID = '569a2001-b87f-490c-92cb-11ba5ea5167c'
-VSP_TX_CHAR_UUID = '569a2000-b87f-490c-92cb-11ba5ea5167c'
+VSP_RX_CHAR_UUID = "569a2001-b87f-490c-92cb-11ba5ea5167c"
+VSP_TX_CHAR_UUID = "569a2000-b87f-490c-92cb-11ba5ea5167c"
 
 _sk = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -42,39 +54,43 @@ class PacketState(Enum):
 
 
 class MoanaBle:
-    def __init__(self, fol, h='hci0'):
+    def __init__(self, fol, g, h="hci0"):
         self.cli = None
         self.packet_state = PacketState.IDLE
         self.packet_length = 0
-        self.packet_length_buff = ''
-        self.packet_address = ''
+        self.packet_length_buff = ""
+        self.packet_address = ""
         self.packet_body = bytearray()
         self.time_sync = None
         self.offload_file = None
-        self.offload_file_name = ''
+        self.offload_file_name = ""
         self.offload_file_size = 0
-        self.offload_file_path = ''
+        self.offload_file_path = ""
         self.offload_file_folder = fol
         self.h = h
-        self.mac = ''
+        self.mac = ""
         os.makedirs(str(self.offload_file_folder), exist_ok=True)
+        self.lat = g[0]
+        self.lon = g[1]
 
     async def packet_write(self, data: str):
         self.packet_reset()
-        length = chr(ord('A') + len(data))
-        await self.cli.write_gatt_char(VSP_RX_CHAR_UUID, f'*{length}{data}'.encode())
+        length = chr(ord("A") + len(data))
+        await self.cli.write_gatt_char(VSP_RX_CHAR_UUID, f"*{length}{data}".encode())
 
     async def packet_complete(self, timeout: int = 60):
-        while (self.packet_state != PacketState.COMPLETE) and \
-                (self.cli and self.cli.is_connected) and \
-                (timeout > 0):
+        while (
+            (self.packet_state != PacketState.COMPLETE)
+            and (self.cli and self.cli.is_connected)
+            and (timeout > 0)
+        ):
             await asyncio.sleep(0.1)
             timeout -= 0.1
 
     async def packet_read(self, address: str):
         await self.packet_complete()
         if self.packet_address != address:
-            return ''
+            return ""
         return self.packet_body.decode()
 
     async def packet_read_binary(self, address: str):
@@ -86,17 +102,17 @@ class MoanaBle:
     def packet_reset(self):
         self.packet_state = PacketState.IDLE
         self.packet_length = 0
-        self.packet_length_buff = ''
-        self.packet_address = ''
+        self.packet_length_buff = ""
+        self.packet_address = ""
         self.packet_body = bytearray()
 
     def packet_parse(self, byte: int):
         if self.packet_state == PacketState.IDLE:
-            if chr(byte) == '*':
+            if chr(byte) == "*":
                 self.packet_state = PacketState.LENGTH
         elif self.packet_state == PacketState.LENGTH:
-            if len(self.packet_length_buff) == 0 and 'A' <= chr(byte) <= 'Z':
-                self.packet_length = byte - ord('A')
+            if len(self.packet_length_buff) == 0 and "A" <= chr(byte) <= "Z":
+                self.packet_length = byte - ord("A")
                 self.packet_state = PacketState.ADDRESS
             else:
                 self.packet_length_buff += chr(byte)
@@ -131,74 +147,82 @@ class MoanaBle:
                 self.packet_parse(i)
 
         try:
-            self.cli = BleakClient(mac, adapter=self.h,
-                                   adapterdisconnected_callback=cb_disc)
+            self.cli = BleakClient(
+                mac, adapter=self.h, adapterdisconnected_callback=cb_disc
+            )
             if await self.cli.connect():
                 self.mac = mac
                 self.cli._mtu_size = 100
                 # print('mtu {}'.format(self.cli.mtu_size))
                 await self.cli.start_notify(VSP_TX_CHAR_UUID, handle_rx)
                 return 0
+
         except (asyncio.TimeoutError, BleakError, OSError) as er:
-            lg.a('error: failed connecting {}'.format(mac))
-            lg.a('error: {}'.format(er))
+            lg.a("error: failed connecting {}".format(mac))
+            lg.a("error: {}".format(er))
         return 1
 
     async def disconnect(self):
         if self.cli and self.cli.is_connected:
-            self.mac = ''
-            print('Disconnecting')
-            await self.packet_write('.')
+            self.mac = ""
+            print("Disconnecting")
+            await self.packet_write(".")
             await self.cli.disconnect()
 
     async def authenticate(self):
-        print('Authenticating')
-        await self.packet_write('A123')
-        if await self.packet_read('a') == '{"Authenticated":true}':
+        print("Authenticating")
+        await self.packet_write("A123")
+        if await self.packet_read("a") == '{"Authenticated":true}':
             return True
-        print('Failed to authenticate')
+        print("Failed to authenticate")
         return False
 
     async def sync_time(self):
-        print('Syncing time')
+        print("Syncing time")
         self.time_sync = int(time.time())
-        await self.packet_write(f'T{self.time_sync}')
-        if await self.packet_read('t') == f'{{"Synchronized":{self.time_sync}}}':
+        await self.packet_write(f"T{self.time_sync}")
+        if await self.packet_read("t") == f'{{"Synchronized":{self.time_sync}}}':
             return True
-        print('Failed to sync time')
+        print("Failed to sync time")
         return False
 
     async def file_info(self):
-        print('Retrieving file info')
-        await self.packet_write('F')
-        rsp = await self.packet_read('F')
-        match = re.search(r'{"FileName":"(.*)","FileSizeEstimate":(.*),"ArchiveBit":"(.*)"}', rsp)
+        print("Retrieving file info")
+        await self.packet_write("F")
+        rsp = await self.packet_read("F")
+        match = re.search(
+            r'{"FileName":"(.*)","FileSizeEstimate":(.*),"ArchiveBit":"(.*)"}', rsp
+        )
         self.offload_file_size = 0
         if match is not None:
             file_name_parts = os.path.splitext(match[1])
-            time_str = datetime.utcfromtimestamp(self.time_sync).strftime('%y%m%d%H%M%S')
+            time_str = datetime.utcfromtimestamp(self.time_sync).strftime(
+                "%y%m%d%H%M%S"
+            )
 
             # ------------------------
             # name of moana file .bin
             # ------------------------
 
-            self.offload_file_name = f'{file_name_parts[0]}_{time_str}.bin'
+            self.offload_file_name = f"{file_name_parts[0]}_{time_str}.bin"
             rsp_as_dict = json.loads(rsp)
-            self.offload_file_size = int(rsp_as_dict['FileSizeEstimate'])
-            print('csv approx. file size', self.offload_file_size)
-            print(f'Offloading to file: {self.offload_file_name}')
+            self.offload_file_size = int(rsp_as_dict["FileSizeEstimate"])
+            print("csv approx. file size", self.offload_file_size)
+            print(f"Offloading to file: {self.offload_file_name}")
             try:
-                self.offload_file_path = str(pathlib.Path(self.offload_file_folder) / self.offload_file_name)
-                self.offload_file = open(self.offload_file_path, 'wb')
+                self.offload_file_path = str(
+                    pathlib.Path(self.offload_file_folder) / self.offload_file_name
+                )
+                self.offload_file = open(self.offload_file_path, "wb")
                 return True
 
             except OSError:
-                print(f'Failed to open offload file {self.offload_file_name}')
+                print(f"Failed to open offload file {self.offload_file_name}")
         return False
 
     async def read_data(self):
-        await self.packet_write('B')
-        data = await self.packet_read_binary('D')
+        await self.packet_write("B")
+        data = await self.packet_read_binary("D")
         if len(data) > 0:
 
             # --------------------------------
@@ -209,18 +233,18 @@ class MoanaBle:
             ble_mat_progress_dl(len(data), self.offload_file_size)
             return False
         else:
-            print(f'\rRead {self.offload_file.tell()} Bytes')
+            print(f"\rRead {self.offload_file.tell()} Bytes")
             self.close_file()
             return True
 
     async def file_checksum(self):
-        print('Checking checksum')
-        await self.packet_write('Z')
-        moana_checksum_str = await self.packet_read('z')
+        print("Checking checksum")
+        await self.packet_write("Z")
+        moana_checksum_str = await self.packet_read("z")
         moana_checksum = int(moana_checksum_str, 16)
         file_checksum = 0
 
-        self.offload_file = open(self.offload_file_path, 'rb')
+        self.offload_file = open(self.offload_file_path, "rb")
         while True:
             byte = self.offload_file.read(1)
             if not byte:
@@ -232,95 +256,110 @@ class MoanaBle:
         self.offload_file.close()
 
         if file_checksum != moana_checksum:
-            print('File checksum does not match')
+            print("File checksum does not match")
             return False
-        print('File checksum matches')
+        print("File checksum matches")
         return True
 
     async def clear_data(self):
-        print('Clearing file from Moana')
-        await self.packet_write('C')
-        return await self.packet_read('c') == '{"ArchiveBit":false}'
+        print("Clearing file from Moana")
+        await self.packet_write("C")
+        return await self.packet_read("c") == '{"ArchiveBit":false}'
 
     def close_file(self):
         if self.offload_file is not None:
             self.offload_file.close()
 
-    def generate_lowell_csv_files(self):
-
-        # todo HP ---> test this file generation thing
+    def _generate_lowell_csv_files(self):
 
         # paths and columns
         dl_file_path = self.offload_file_path
-        p = dl_file_path.replace('.bin', '.csv')
-        v = str(get_dl_folder_path_from_mac(self.mac)) + '/' + self.offload_file_name
+        p = dl_file_path.replace(".bin", ".csv")
         create_folder_logger_by_mac(self.mac)
-        pt = v + p[:-4] + '_Temperature.csv'
-        pp = v + p[:-4] + '_Pressure.csv'
-        cols = 'Date,Time,Depth Decibar,Temperature C\n'
+        pt = p[:-4] + "_Temperature.csv"
+        pp = p[:-4] + "_Pressure.csv"
+        cols = "Date,Time,Depth Decibar,Temperature C\n"
 
         # open source and destination files
-        with open(p, 'r') as f:
+        with open(p, "r") as f:
             lines = f.readlines()
-        ft = open(pt, 'w')
-        fp = open(pp, 'w')
-        ft.write('ISO 8601 Time, Temperature (C)\n')
-        fp.write('ISO 8601 Time, Pressure (dbar)\n')
+        ft = open(pt, "w")
+        fp = open(pp, "w")
+        ft.write("ISO 8601 Time,Temperature (C)\n")
+        fp.write("ISO 8601 Time,Pressure (dbar)\n")
 
         # grab only the data rows of source file
         idx_data = lines.index(cols)
-        lines_data = lines[idx_data + 1:]
+        lines_data = lines[idx_data + 1 :]
         for i in lines_data:
-            mmddyyyy, hhmmss, p, t = i.split(',')
-            mm, dd, yyyy = mmddyyyy.split('/')
-            _ = '{}-{}-{}T{}.000'.format(yyyy, mm, dd, hhmmss)
+            i = i.replace("\n", "")
+            mmddyyyy, hhmmss, p, t = i.split(",")
+            mm, dd, yyyy = mmddyyyy.split("/")
+            _ = "{}-{}-{}T{}.000".format(yyyy, mm, dd, hhmmss)
             # split and write proper data rows to destination files
-            ft.write('{},{}\n'.format(_, t))
-            fp.write('{},{}\n'.format(_, p))
+            ft.write("{},{}\n".format(_, t))
+            fp.write("{},{}\n".format(_, p))
         ft.close()
         fp.close()
 
+    def generate_lowell_csv_files(self):
+        try:
+            return self._generate_lowell_csv_files()
+        except (Exception,) as ex:
+            lg.a("error: generate li csv files for moana -> {}".format(ex))
+
     def decode(self):
         dl_file_path = self.offload_file_path
-        csv_file_path = dl_file_path.replace('.bin', '.csv')
+        csv_file_path = dl_file_path.replace(".bin", ".csv")
 
-        with open(dl_file_path, 'rb') as bin_file:
-            print(f'Decoding to file: {csv_file_path}')
-            with open(csv_file_path, 'wb') as csv_file:
+        with open(dl_file_path, "rb") as bin_file:
+            print(f"Decoding to file: {csv_file_path}")
+            with open(csv_file_path, "wb") as csv_file:
                 # header
                 data = bin_file.read(1)
-                while data and data != b'\x03':
+                while data and data != b"\x03":
                     csv_file.write(data)
                     data = bin_file.read(1)
 
                 # origin time
-                timestamp = struct.unpack('<i', bin_file.read(4))[0]
+                timestamp = struct.unpack("<i", bin_file.read(4))[0]
 
                 data = bin_file.read(6)
                 while data:
                     if len(data) != 6:
-                        print('Unexpected number of bytes')
+                        print("Unexpected number of bytes")
                         break
-                    values = struct.unpack('<3H', data)
+                    values = struct.unpack("<3H", data)
 
                     timestamp += values[0]
-                    csv_file.write(f'{datetime.utcfromtimestamp(timestamp).strftime("%d/%m/%Y,%H:%M:%S")},'.encode())
+                    csv_file.write(
+                        f'{datetime.utcfromtimestamp(timestamp).strftime("%d/%m/%Y,%H:%M:%S")},'.encode()
+                    )
 
                     depth = (values[1] / 10) - 10
-                    csv_file.write(f'{depth:.1f},'.encode())
+                    csv_file.write(f"{depth:.1f},".encode())
 
                     temp = (values[2] / 1000) - 10
-                    csv_file.write(f'{temp:.3f}\n'.encode())
+                    csv_file.write(f"{temp:.3f}\n".encode())
 
                     data = bin_file.read(6)
-        print(f'Decoding finished')
+        print("Decoding finished")
 
     async def download_recipe(self, mac):
 
-        rv = await self.connect(mac)
-        if rv:
-            print('moana could not connect to {}'.format(mac))
-            return False
+        till = time.perf_counter() + 30
+        while 1:
+            if time.perf_counter() > till:
+                lg.a("moana timeout! could not connect to {}".format(mac))
+                return False
+            try:
+                if await self.connect(mac) == 0:
+                    # going well :)
+                    break
+            except (Exception,) as ex:
+                lg.a("error: moana connect exception {}".format(ex))
+                time.sleep(1)
+
         status = True
         last_state = OffloadState.CONNECT
         offload_state = OffloadState.AUTHENTICATE
@@ -332,11 +371,11 @@ class MoanaBle:
                 state_time = datetime.now()
             else:
                 if (datetime.now() - state_time).total_seconds() > 600:
-                    print('Timed out during offload')
+                    print("Timed out during offload")
                     break
 
             if offload_state == OffloadState.AUTHENTICATE:
-                print('Status file changed to 0')
+                print("Status file changed to 0")
                 status = await self.authenticate()
                 offload_state = OffloadState.SYNC_TIME
             elif offload_state == OffloadState.SYNC_TIME:
@@ -358,50 +397,59 @@ class MoanaBle:
                 break
 
         if offload_state == OffloadState.COMPLETE:
+
+            # ---------------------------------
+            # generates Moana output CSV file
+            # ---------------------------------
             self.decode()
+
+            # ----------------------------------
+            # generates custom output CSV files
+            # ----------------------------------
+            if ddh_is_emolt_box() or not linux_is_rpi():
+                lg.a("emolt box detected, converting Moana CSV file to emolt format")
+                file_emolt_zt_csv = file_moana_raw_csv_to_emolt_csv(
+                    self.offload_file_path, self.lat, self.lon
+                )
+                hl_csv_file = file_emolt_csv_to_hl(
+                    file_emolt_zt_csv, logger_type="moana"
+                )
+                x85 = file_out_hl_process_xc_85(hl_csv_file)
+                ms = rbl_build_emolt_msg_as_str(self.lat, self.lon, x85)
+                mb = rbl_hex_str_to_hex_bytes(ms)
+                rbl_gen_file(mb)
+
+            # Lowell files always generated, needed for plotting at least
+            lg.a("NOT emolt box detecting, converting file to LI format")
             self.generate_lowell_csv_files()
+
+            # indicate 100% download
             v = self.offload_file_size
-            # force 100%
             ble_mat_progress_dl(v, v)
             time.sleep(5)
-            print('Offload succeeded')
+            print("Offload succeeded")
             return True
 
         # went south
         await self.disconnect()
         self.close_file()
-        print('Offload failed')
-
+        print("Offload failed")
         ble_mat_progress_dl(0, self.offload_file_size)
         return False
 
 
-async def ble_interact_moana(dl_folder, mac, h):
-    lc = MoanaBle(dl_folder, h)
+async def ble_interact_moana(dl_folder, mac, h, g):
+    lc = MoanaBle(dl_folder, g, h)
     try:
-        lg.a('interacting with Moana logger, mac {}'.format(mac))
+        lg.a("interacting with Moana logger, mac {}".format(mac))
         if lc:
-            await lc.download_recipe(mac)
+            rv = await lc.download_recipe(mac)
+            if not rv:
+                return 1
             return 0
 
     except Exception as ex:
-        print('[ BLE ] dl_moana_exception', ex)
+        lg.a("error: dl_moana_exception -> {}".format(ex))
+        lg.a("error: traceback -> {}".format(traceback.print_exc()))
         await lc.disconnect()
         return 1
-
-
-# ------
-# test
-# ------
-def main_moana(fol, _mac):
-    # fol: dl_files_folder_path
-    _h = 'hci0'
-    _args = [fol, _mac, _h]
-    ael.run_until_complete(ble_interact_moana(*_args))
-
-
-if __name__ == '__main__':
-    _mac = 'C9:3C:F8:37:E9:6A'
-    d = get_ddh_folder_path_dl_files()
-    d = str(d / _mac).replace(':', '-')
-    main_moana(d, _mac)
