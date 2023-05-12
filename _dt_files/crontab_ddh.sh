4804fa78feee22ddc2cf50d6185bf1be802d53fa
kazola <kazola>
2023-05-12 11:21:05 -0400

    more

--- _dt_files/crontab_ddh.sh
+++ _dt_files/crontab_ddh.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
 
+# needed for crontab to access the X-window system
+export XAUTHORITY=/home/pi/.Xauthority
+export DISPLAY=:0
+
+
 /home/pi/li/ddt/dt_install_python_ddh.sh
 /home/pi/li/ddh/run_ddh.sh&
 /home/pi/li/ddh/run_dds.sh&
