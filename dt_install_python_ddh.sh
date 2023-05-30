e3276c8898c212c777748363f370ba7843e736f4
kaz <kaz>
2023-05-30 11:04:26 -0400

    updater

--- dt_install_python_ddh.sh
+++ dt_install_python_ddh.sh
@@ -229,7 +229,7 @@ _install() {
         COM_MAT_LOC=$(cd "$F_CLONE_MAT" && git rev-parse master); rv=$?;
         if [ "$rv" -ne 0 ]; then _e "cannot get MAT local version"; fi
         if [ ${#COM_DDH_LOC} -ne 40 ]; then _e "bad MAT local version"; fi
-        sudo echo "$COM_MAT_LOC" > /etc/com_mat_loc.txt
+        sudo sh -c 'echo $COM_MAT_LOC > /etc/com_mat_loc.txt'
     fi
 
     _s "VENV - cloning DDH"
