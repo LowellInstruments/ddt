04ab33fe23519ad720d50d6066db6b359fb44e43
kaz <kaz>
2023-10-10 09:19:06 -0400

    more

--- dt_install_utils.sh
+++ dt_install_utils.sh
@@ -3,19 +3,21 @@
 
 F_LI=/home/pi/li
 F_DT="$F_LI"/ddt
+F_IN=/usr/local/bin
 
 
 printf '\n\n\n---- dt_install_utils ----\n'
 
 
 printf '\n\n>>> installing climenu\n'
-sudo cp $F_DT/_dt_files/cm /usr/local/bin
+sudo cp $F_DT/_dt_files/cm $F_IN
 sudo cp $F_DT/_dt_files/cm_ddh.conf /etc/
 grep 'alias cm' /home/pi/.bashrc
 rv=$?
 if [ $rv -ne 0 ]; then
-    echo 'alias cm="/usr/local/bin/cm"' >> /home/pi/.bashrc
+    echo 'alias cm="/usr/local/bin/cm /etc/cm_ddh.conf"' >> /home/pi/.bashrc
 fi
+sudo chmod +x $F_IN/cm
 
 
 printf '\n\n>>> dt_install_utils = OK\n'
