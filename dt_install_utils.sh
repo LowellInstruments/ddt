154d017f420c5f00c5ddd86d28db4fbb003413d5
kaz <kaz>
2023-10-10 09:16:23 -0400

    more

--- dt_install_utils.sh
+++ dt_install_utils.sh
@@ -17,7 +17,7 @@ printf '\n\n\n---- dt_install_utils ----\n'
 printf '\n\n>>> installing climenu\n'
 sudo cp $F_DT/_dt_files/cm /usr/local/bin
 sudo cp $F_DT/_dt_files/cm_ddh.conf /etc/
-grep 'alias cm' /home/kaz/.bashrc
+grep 'alias cm' /home/pi/.bashrc
 rv=$?
 if [ $rv -ne 0 ]; then
     echo 'alias cm="/usr/local/bin/cm"' >> /home/pi/.bashrc
