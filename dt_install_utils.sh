853a953bc5fc056fa686057dc11665de856932b0
kaz <kaz>
2023-10-10 09:15:23 -0400

    cm

---
+++ dt_install_utils.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+
+F_LI=/home/pi/li
+F_DT="$F_LI"/ddt
+
+
+# abort upon any error
+clear && echo && set -e
+trap 'echo "$BASH_COMMAND" TRAPPED! rv $?' EXIT
+if [ "$PWD" != "$F_DT" ]; then echo 'wrong starting folder'; exit 1; fi
+
+
+printf '\n\n\n---- dt_install_utils ----\n'
+
+
+printf '\n\n>>> installing climenu\n'
+sudo cp $F_DT/_dt_files/cm /usr/local/bin
+sudo cp $F_DT/_dt_files/cm_ddh.conf /etc/
+grep 'alias cm' /home/kaz/.bashrc
+rv=$?
+if [ $rv -ne 0 ]; then
+    echo 'alias cm="/usr/local/bin/cm"' >> /home/pi/.bashrc
+fi
+
+
+printf '\n\n>>> dt_install_utils = OK\n'
