161d60302c9a5d5c4e17d794600327f4fa2f15c3
kaz <kaz>
2023-11-27 09:38:46 -0500

    more

--- dt_install_icons.sh
+++ dt_install_icons.sh
@@ -2,18 +2,16 @@
 
 
 F_DT=/home/pi/li/ddt
-F_DA=/home/pi/li/ddh
-
 
 clear
 echo; echo; echo;
 
 echo '> installing icon run DDH'
 cp "$F_DT"/_dt_files/run_ddh_from_desktop.sh /home/pi/Desktop
-echo '> installing icon script GPS'
-cp "$F_DA"/scripts/run_gps_test_from_desktop.sh /home/pi/Desktop
-echo '> installing icon script deploy loggers'
-cp "$F_DA"/scripts/run_script_logger_do_deploy.sh /home/pi/Desktop
+
+# other icons such as the following are now part of 'ddc'
+#     - GPS test
+#     - run script to deploy loggers
 
 echo; echo; echo
 
