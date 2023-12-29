3ac32e15e9b49a10d4344aaabf47d494952930a2
kaz <kaz@kaz.com>
2023-12-29 14:36:58 -0500

    installer

--- dt_install_all.sh
+++ dt_install_all.sh
@@ -9,7 +9,6 @@ source dt_install_custom_box.sh
 source dt_install_linux.sh
 source dt_install_linux_bluez.sh
 source dt_install_ddh.sh
-_dt_files/ppp_install_custom.sh
 source dt_install_service_sw_net.sh
 source dt_install_alias.sh
 source dt_install_icons.sh
@@ -20,6 +19,7 @@ install_custom
 install_linux
 install_bluez
 install_ddh
+_dt_files/ppp_install_custom.sh
 install_service_sw_net
 install_alias
 install_icons
