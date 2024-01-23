ffae9756c77624c5bf126ee96426f9b6f418ed2e
kaz <kaz>
2024-01-23 13:15:20 -0500

    warning ddh 2023

--- dt_check.sh
+++ dt_check.sh
@@ -6,3 +6,13 @@ if [ "$(pwd)" != $FOL ]; then
     echo "error dt_check: working_dir should be $FOL"
     exit 1
 fi
+
+
+grep 2023 /boot/issue.txt
+rv=$?
+if [ $rv -ne 0 ]; then
+    # example: Raspberry Pi reference 2023-05-03
+    echo "-----------------------------"
+    echo "warning: this DDH is not 2023"
+    echo "-----------------------------"
+fi
