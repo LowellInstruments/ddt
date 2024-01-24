42e98f30d3e0f0226c827188b3637cb671749078
kaz <kaz>
2024-01-24 15:03:32 -0500

    more

--- dt_install_vpn.sh
+++ dt_install_vpn.sh
@@ -35,37 +35,33 @@ function install_vpn {
     fi
 
 
-    printf "\n"
-    _pb "# ----------------------------------------------\n"
-    _pb "# creating this peer's /etc/wireguard/wg0.conf\n"
-    _pb "# ----------------------------------------------\n"
-    printf "\n"
+    _pb "# creating this peer's /etc/wireguard/wg0.conf"
     if [ $is_rpi -eq 0 ]; then
         _f=/etc/wireguard/wg0.conf
-        sudo echo -e "# file auto-created by Lowell Instruments tool" | sudo tee $_f > /dev/null
-        sudo echo -e  "----------------------------------------------" | sudo tee $_f > /dev/null
-        sudo echo -e  "# info about myself as a peer" | sudo tee $_f > /dev/null
-        sudo echo -e  "[Interface]" | sudo tee $_f > /dev/null
-        sudo printf   "Address = %s/32\n" "$2" | sudo tee $_f > /dev/null
-        sudo printf   "PrivateKey = %s \n\n" "$(cat "$VF"/$NAME.key)" | sudo tee $_f > /dev/null
-        sudo echo -e  "\t# info about the Hub" | sudo tee $_f > /dev/null
-        sudo echo -e  "\t[Peer]" | sudo tee $_f > /dev/null
-        sudo printf   "\tPublicKey = %s\n" "$1" | sudo tee $_f > /dev/null
-        sudo echo -e  "\tEndpoint = 3.143.21.254:51820" | sudo tee $_f > /dev/null
-        sudo echo -e  "\t# set hosts allowed to reach this peer via HUB" | sudo tee $_f > /dev/null
-        sudo echo -e  "\tAllowedIPs = 10.5.0.0/24" | sudo tee $_f > /dev/null
-        sudo echo -e  "\tPersistentKeepalive = 25" | sudo tee $_f > /dev/null
+        (sudo echo -e "# file auto-created by Lowell Instruments tool"; \
+        sudo echo -e  "----------------------------------------------"; \
+        sudo echo -e  "# info about myself as a peer"; \
+        sudo echo -e  "[Interface]"; \
+        sudo printf   "Address = %s/32\n" "$2"; \
+        sudo printf   "PrivateKey = %s \n\n" "$(cat "$VF"/$NAME.key)"; \
+        sudo echo -e  "\t# info about the Hub"; \
+        sudo echo -e  "\t[Peer]"; \
+        sudo printf   "\tPublicKey = %s\n" "$1"; \
+        sudo echo -e  "\tEndpoint = 3.143.21.254:51820"; \
+        sudo echo -e  "\t# set hosts allowed to reach this peer via HUB"; \
+        sudo echo -e  "\tAllowedIPs = 10.5.0.0/24"; \
+        sudo echo -e  "\tPersistentKeepalive = 25";) | sudo tee $_f > /dev/null
     fi
     printf "\n\n\n"
-    _pb "# ----------------------------------------------\n"
-    _pb "# paste to the Hub's /etc/wireguard/wg0.conf\n"
-    _pb "# ----------------------------------------------\n"
+    _pb "# ----------------------------------------------"
+    _pb "# copy-paste to the Hub's /etc/wireguard/wg0.conf"
+    _pb "# ----------------------------------------------"
     printf "\n"
     printf "# info about the remote peer\n"
     printf "\t[Peer]\n"
     printf "\tPublicKey = %s\n" "$(cat "$VF"/$NAME.pub)"
-    printf "\tAllowedIPs = %s/32\n" "$2" >> pepi.txt
-
+    printf "\tAllowedIPs = %s/32\n" "$2"
+    printf "\n"
 
     # permissions
     if [ $is_rpi -eq 0 ]; then
