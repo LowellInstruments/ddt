17d79bd92339f30143f62cc88c66c48e5cab17c2
kaz <kaz>
2024-01-25 14:56:11 -0500

    mroe

--- dt_install_config.sh
+++ dt_install_config.sh
@@ -5,11 +5,6 @@
 VPN_ADDR="ip a s wg0 | grep -E -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2"
 
 
-# this script does the following:
-#   1) obtain own DDH VPN interface IP address (va)
-#   2) use (va) to query DDN API to get a DDH config.toml file
-
-
 function read_api_pw_to_file {
     source dt_utils.sh
     read -rp "enter api pw: " api_pw
@@ -18,9 +13,12 @@ function read_api_pw_to_file {
 }
 
 
-function install_config {
-    source dt_utils.sh
-    _pb "INSTALL DDH CONFIG"
-}
+# this script has a slightly different structure
+clear && echo
+_pb "running dt_install_config.sh"
 
 
+# step 1) call python script to get config.toml file
+# password must be previously stored in file $HOME/.api_pw
+api_pw=$(cat "$HOME"/.api_pw)
+python3 _dt_files/_ddn_cli_api_cfg.py "$api_pw"
