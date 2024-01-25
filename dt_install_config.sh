6abb745a8997fd364b01055dcfdb8dc2a7c55dbb
kaz <kaz>
2024-01-25 14:27:22 -0500

    api_pw

--- dt_install_config.sh
+++ dt_install_config.sh
@@ -2,13 +2,20 @@
 
 
 
-VPN_ADDR=ip a s wg0 | grep -E -o \
-'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2
+VPN_ADDR="ip a s wg0 | grep -E -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2"
 
 
 # this script does the following:
 #   1) obtain own DDH VPN interface IP address (va)
 #   2) use (va) to query DDN API to get a DDH config.toml file
+
+
+function read_api_pw_to_file {
+    source dt_utils.sh
+    read -rp "enter api pw: " api_pw
+    # shellcheck disable=SC2086
+    echo "$api_pw" > "$HOME"/.api_pw
+}
 
 
 function install_config {
@@ -17,4 +24,4 @@ function install_config {
 }
 
 
-install_config
+read_api_pw_to_file
