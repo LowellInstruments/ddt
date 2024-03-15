359f63cd12da800bd2bfcb3b696273329ca2583a
kaz <kaz>
2024-03-15 15:30:20 -0400

    more

--- dt_install_icons.sh
+++ dt_install_icons.sh
@@ -10,8 +10,8 @@ function install_icons {
     cd "$FOL_LI" || (_pe "error: bad working directory"; exit 1)
 
 
-    cp "$FOL_DDT"/_dt_files/crontab_ddh.sh /home/pi/Desktop
-    cp "$FOL_DDT"/_dt_files/run_ddc_from_desktop.sh /home/pi/Desktop/conf_tool_ddh.sh
+    cp "$FOL_DDT"/_dt_files/crontab_ddh.sh /home/pi/Desktop/DDH.sh
+    cp "$FOL_DDT"/_dt_files/run_ddc_from_desktop.sh /home/pi/Desktop/DDC.sh
     _e $? "install_icons"
 }
 
