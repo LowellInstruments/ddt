24599a0d3f6bca6cebb96efa46646595ec2777a3
kaz <kaz>
2024-03-25 12:25:33 -0400

    more

--- _dt_files/mem_log.sh
+++ _dt_files/mem_log.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-FO=usage_mem.log
+FO=/home/pi/li/ddt/_dt_files/usage_mem.log
 
-while true; do
-	top -bn 1 -o +%MEM | grep "load average" -A 9 | tee -a $FO
-	echo | tee -a $FO
-	sleep 5
-done
+#while true; do
+#       top -bn 1 -o +%MEM | grep "load average" -A 9 | tee -a $FO
+#       echo | tee -a $FO
+#       sleep 5
+#done
+
+
+# when run via crontab, only once
+# */5 * * * * pi /home/pi/li/ddt/_dt_files/mem_log.sh
+top -bn 1 -o +%MEM | grep "load average" -A 9 | tee -a $FO
