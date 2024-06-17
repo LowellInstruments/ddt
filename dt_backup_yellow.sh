3400ceac5d1bd73c9c99884eaff106674178ba24
kaz <kaz>
2024-06-17 18:05:47 +0200

    backup_yellow

--- dt_backup_yellow.sh
+++ dt_backup_yellow.sh
@@ -1,35 +1,33 @@
 #!/usr/bin/env bash
-source dt_utils.sh
+
+FOL_LI=/home/pi/li
+FOL_DDH=$FOL_LI/ddh
 
 
-
-function backup_yellow {
-    title dt_backup_yellow
-
-
-    _pb "BACKUP DDH YELLOW VERSION"
-    cd "$FOL_LI" || (_pe "error: bad working directory"; exit 1)
+clear
+echo "BACKUP DDH YELLOW VERSION"
+cd "$FOL_LI" || (echo "error: bad working directory"; exit 1)
 
 
-    _pb "detecting old yellow boat DDH version"
-    [ -f /home/pi/li/ddh/settings/ddh.json ]
-    rv=$?
-    if [ $rv -eq 0 ]; then
-        _pb "creating old yellow boat DDH folder backup to ddh_yellow"
-        mv "$FOL_DDH" "$FOL_LI"/ddh_yellow
-    fi
+echo "detecting old yellow boat DDH version"
+[ -f $FOL_DDH/settings/ddh.json ]
+rv=$?
+if [ $rv -eq 0 ]; then
+    echo "creating old yellow boat DDH folder backup to ddh_yellow"
+    mv "$FOL_DDH" "$FOL_LI"/ddh_yellow
+fi
 
 
-    _pb "killing any currently running DDH software"
-    killall main_ddh_controller
-    killall main_ddh
-    kilall main_dds_controller
-    killall main_dds
+echo "killing any currently running DDH software"
+killall main_ddh_controller
+killall main_ddh
+killall main_dds_controller
+killall main_dds
 
 
-    _pb "backup_yellow done, proceed to run ./dt_install_all.sh"
-}
+echo "cloning latest DDT"
+rm -rf /home/pi/li/ddt
+git clone https://github.com/lowellinstruments/ddt.git -b toml
 
 
-if [ "$1" == "force" ]; then backup_yellow; fi
-
+echo "backup_yellow done, proceed to run ./dt_install_all.sh"
