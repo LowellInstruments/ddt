4a8924e4a264044069e68d87cbd32a3065fb8f0b
kaz <kaz>
2024-07-01 10:26:21 -0400

    toml

--- _dt_files/crontab_ddh.sh
+++ _dt_files/crontab_ddh.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
 
+# don't run if BRT tool is running
+ps -aux | grep brt | grep -v grep
+rv=$?
+if [ $rv -eq 0 ]; then
+    printf "detected BRT running, DDH leaving\n"
+    exit 0
+fi
+
+
+
 /home/pi/li/ddh/run_dds.sh&
 
 # prevents mangled output at start
