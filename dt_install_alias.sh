a0c4c675b162f9ba229c4847de19820ff22c831b
kaz <kaz>
2024-09-25 11:19:37 -0400

    toml

--- dt_install_alias.sh
+++ dt_install_alias.sh
@@ -14,20 +14,28 @@ function install_alias {
     cd "$FOL_LI" || (_pe "error: bad working directory"; exit 1)
 
 
-    _pb "climenu binary build"
-    gcc "$FOL_DDT"/_dt_files/climenu.c -o "$FOL_DDT"/_dt_files/cm
-    _e $? "building climenu"
+
+    # convenient aliases
+    _pb "alias shortcut"
+    grep 'alias cdh' $F_RC
+    rv=$?
+    if [ $rv -ne 0 ]; then
+        echo "alias cdh=\"cd /home/pi/li/ddh\"" >> $F_RC
+    fi
+    grep 'alias cdt' $F_RC
+    rv=$?
+    if [ $rv -ne 0 ]; then
+        echo "alias cdt=\"cd /home/pi/li/ddt\"" >> $F_RC
+    fi
+    grep 'alias mnc' $F_RC
+    rv=$?
+    if [ $rv -ne 0 ]; then
+        echo "alias mnc=\"minicom -D /dev/ttyUSB2\"" >> $F_RC
+    fi
 
 
-    _pb "climenu install"
-    sudo killall cm 2> /dev/null
-    sudo cp "$FOL_DDT"/_dt_files/cm $F_IN && \
-    sudo cp "$FOL_DDT"/_dt_files/cmi.conf /etc && \
-    sudo cp "$FOL_DDT"/_dt_files/cmu.conf /etc
-    _e $? "copy alias"
 
-
-    _pb "climenu alias"
+    _pb "alias ddc"
     sed -i '/alias ddc/d' $F_RC
     grep 'alias ddc' $F_RC
     rv=$?
@@ -35,44 +43,31 @@ function install_alias {
         echo "alias ddc=\"/home/pi/li/ddh/run_ddc.sh\"" >> $F_RC
     fi
 
+
+    _pb "climenu build and install"
+    gcc "$FOL_DDT"/_dt_files/climenu.c -o "$FOL_DDT"/_dt_files/cm
+    _e $? "climenu build"
+    sudo killall cm 2> /dev/null
+    sudo cp "$FOL_DDT"/_dt_files/cm $F_IN && \
+    sudo cp "$FOL_DDT"/_dt_files/cmi.conf /etc && \
+    sudo cp "$FOL_DDT"/_dt_files/cmu.conf /etc
+    _e $? "climenu install"
+
+
+
+    _pb "alias climenu ddi, ddu"
     grep 'alias ddi' $F_RC
     rv=$?
     if [ $rv -ne 0 ]; then
         echo "alias ddi=\"$F_IN/cm /etc/cmi.conf\"" >> $F_RC
     fi
-
     grep 'alias ddu' $F_RC
     rv=$?
     if [ $rv -ne 0 ]; then
         echo "alias ddu=\"$F_IN/cm /etc/cmu.conf\"" >> $F_RC
     fi
     sudo chmod +x $F_IN/cm
-    _e $? "install alias"
-
-    # ---------------------
-    # not used very often
-    # ---------------------
-
-    # alias configuration edit
-    grep 'alias ace' $F_RC
-    rv=$?
-    if [ $rv -ne 0 ]; then
-        echo "alias ace=\"nano $FOL_DDH/settings/config.toml\"" >> $F_RC
-    fi
-
-    # alias configuration see
-    grep 'alias acs' $F_RC
-    rv=$?
-    if [ $rv -ne 0 ]; then
-        echo "alias acs=\"cat $FOL_DDH/settings/config.toml\"" >> $F_RC
-    fi
-
-    # alias cronTab edit
-    grep 'alias ate' $F_RC
-    rv=$?
-    if [ $rv -ne 0 ]; then
-        echo "alias ate=\"sudo nano /etc/crontab\"" >> $F_RC
-    fi
+    _e $? "install climenu alias"
 }
 
 
