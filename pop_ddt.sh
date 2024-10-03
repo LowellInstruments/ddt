b5dab2c9893845f1f70177a377c081866e96f88c
kaz <kaz>
2024-10-03 09:23:34 -0400

    toml

--- pop_ddt.sh
+++ pop_ddt.sh
@@ -6,24 +6,24 @@ source /home/pi/li/ddh/scripts/utils.sh
 clear
 
 
-_S="[ POP ] ddt | updating"
+_S="[ DDU ] updating DDT"
 _pb "$_S"
 cd "$FOL_DDT" && git reset --hard && git pull
 _e $? "$_S"
 
 
-_S="[ POP ] ddt | new aliases"
+_S="[ DDU ] creating new aliases for DDT"
 _pb "$_S"
 cd "$FOL_DDT" && ./dt_install_alias.sh force
 _e $? "$_S"
 
 
-_S="[ POP ] ddt | sourcing bashrc"
+_S="[ DDU ] sourcing bashrc for DDT"
 _pb "$_S"
 source /home/pi/.bashrc
 _e $? "$_S"
 
 
 
-_pb "[ POP ] ddt | done OK!"
+_pb "[ DDU ] OK, done"
 
