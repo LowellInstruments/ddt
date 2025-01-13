e2b52dac85a31ef836f4c2e3721bfe3d4a81737e
kaz <kaz>
2025-01-13 12:30:10 -0500

    toml

--- dt_install_power.sh
+++ dt_install_power.sh
@@ -60,13 +60,13 @@ function install_power {
 
 
         _pb "modifying sailorhat couple of files"
-        cp "$FOL_DDT"/_dt_files/sailor_const.py /tmp/my_sailorhat/SH-RPi-daemon/src/shrpi/const.py
-        cp "$FOL_DDT"/_dt_files/sailor_sm.py /tmp/my_sailorhat/SH-RPi-daemon/src/shrpi/state_machine.py
+        cp "$FOL_DDT"/_dt_files/sailor_const.py $FOL_TMP_SAH/SH-RPi-daemon/src/shrpi/const.py
+        cp "$FOL_DDT"/_dt_files/sailor_sm.py $FOL_TMP_SAH/SH-RPi-daemon/src/shrpi/state_machine.py
         _e $? "modifying sailor_hat"
 
 
         _pb "installing modified sailorhat"
-        cd $FOL_TMP_SAH && \
+        cd $FOL_TMP_SAH/SH-RPi-daemon && \
         sudo ./install.sh --enable RTC
         _e $? "installing sailor_hat"
 
