26b37ba4e5f2290cdda4062c8bb85631020ed409
kaz <kaz>
2025-01-13 12:25:56 -0500

    toml

--- dt_install_custom_box.sh
+++ dt_install_custom_box.sh
@@ -28,29 +28,28 @@ function install_custom {
 
     # how DDH groups files before S3 upload
     echo
-    read -rp "Set this DDH with grouped S3 uplink? (y/n) " choice
+    read -rp "Set DDH AWS GROUPed S3 uplink? (y/n) " choice
     case "$choice" in
         y|Y ) touch "$GROUPED_S3_FILE_FLAG"; printf 'set grouped S3 flag OK\n';;
     esac
 
 
     echo
-    read -rp "Set this DDH with external GPS puck? (y/n) " choice
+    read -rp "Set DDH with external GPS puck? (y/n) " choice
     case "$choice" in
         y|Y ) touch "$GPS_EXTERNAL_FILE_FLAG"; printf 'set GPS puck flag OK\n';;
     esac
 
 
-
     echo
-    read -rp "Does this DDH use a cell shield with TWILIO SIM? (y/n) " choice
+    read -rp "Set DDH cell with TWILIO SIM? (y/n) " choice
     case "$choice" in
         y|Y ) touch "$DDH_USES_SHIELD_CELL_TWILIO"; printf 'set cell shield with twilio flag OK\n';;
     esac
 
 
     echo
-    read -rp "Does this DDH use a cell shield with SIXFAB SIM? (y/n) " choice
+    read -rp "Set DDH cell with SIXFAB SIM? (y/n) " choice
     case "$choice" in
         y|Y ) touch "$DDH_USES_SHIELD_CELL_SIXFAB"; printf 'set cell shield with sixfab flag OK\n';;
     esac
