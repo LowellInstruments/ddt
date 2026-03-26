cb80e9ee38fa841ae470e11e6672a10713b0e592
kaz <kaz>
2026-03-26 16:01:26 -0400

    lxp

--- _dt_files/crontab_lxp.sh
+++ _dt_files/crontab_lxp.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
 
+
+# get rid of possible nomachine update pop-ups
+sudo kill -9 `ps -aux | grep NX | grep update | grep -v grep | awk '{print $2}'`
+
+
+# check LXPanel is not consuming too much CPU / RAM
 /home/pi/li/ddh/run_lxp.sh
 
