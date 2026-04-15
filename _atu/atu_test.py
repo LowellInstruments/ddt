7ab90a816ea81a6f1994fc9029b5f45626be2e05
kaz <kaz>
2026-04-15 16:26:25 -0400

    more

---
+++ _atu/atu_test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+
+from _atu_cmd import cmd
+from atu_inv_all import ping_all
+
+
+
+if __name__ == '__main__':
+    timeout = 1
+    ls = ping_all()
+    for i in ls:
+        c = 'cat /home/pi/li/ddh/.ddh_version'
+        cmd('test', c, timeout)
+
