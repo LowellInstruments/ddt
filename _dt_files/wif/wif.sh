78bf69ec4b2ee3721c3100dc33c68c6c38d06b5d
kaz <kaz>
2026-05-19 11:11:27 -0400

    sailorhat

---
+++ _dt_files/wif/wif.sh
@@ -0,0 +1,15 @@
+
+cd /etc/NetworkManager/system-connections/
+
+# step 1 - create a file.nmconnection, set priority in [connection] section
+autoconnect-priority=100
+chmod 0600 file.nmconnection
+
+# step 2 - import newly created files
+nmcli conn reload
+nmcli -f name,autoconnect,autoconnect-priority connection show
+sudo systemctl restart NetworkManager
+
+reboot
+
+
