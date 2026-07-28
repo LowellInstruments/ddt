9a507db769deeacd4f8ac9669b5bfb6022e7343c
kaz <kaz>
2026-07-28 13:29:03 -0400

    banner

--- pop_ddh.sh
+++ pop_ddh.sh
@@ -55,6 +55,9 @@ if [ $rv -ne 0 ]; then
     # we have no UV, just pip then, slower
     # --------------------------------------
     BIN_PIP="$FOL_VEN/bin/pip -q"
+    say "Using standard PIP"
+else
+    say "Using UV pip"
 fi
 
 
